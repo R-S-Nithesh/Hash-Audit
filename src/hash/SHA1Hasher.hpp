0953458059e6ad214f90fe98dc11f9a68cf64d9a
R.S.Nithesh <rsnithesh2004@gmail.com>
2026-06-15 19:23:21 +0530

    Add files via upload

---
+++ src/hash/SHA1Hasher.hpp
@@ -0,0 +1,11 @@
+/*
+SHA1Hasher.hpp
+
+Forwarding header exposing SHA1Hasher from EVPHasher.hpp
+
+Connects to:
+  hash/EVPHasher.hpp - defines SHA1Hasher as EVPHasher<SHA1Tag>
+*/
+
+#pragma once
+#include "src/hash/EVPHasher.hpp"
